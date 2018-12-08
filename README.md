6f1f819cf66ac4fa9e6b9ad1ee9085079f812f11
Masashi <masashi.sode@gmail.com>
2018-12-08 16:48:40 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# PyASOpt
+
+## Install
+
+`python setup.py install`
+
+## Usage
+
+```python
+from PyASOpt.ASOpt import ASOpt
+Opt = ASOpt()
+```
+under construction
+
+## Examples
+
+under construction  
+coming soon...  
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+# import sys
+# if sys.version_info >= (3,7):
+#     sys.exit('Sorry, Python >= 3.7 is not supported')
+
+setup(
+    name='pyASOpt',
+    python_requires='<3.7',
+    version='0.0.1',
+    description='Sample package for Python-Guide.org',
+    # long_description=readme,
+    author='Masashi Sode',
+    author_email='masashi.sode@gmail.com',
+    install_requires=['numpy', 'pandas',
+                      'pynastran', 'pydoe',
+                      'matplotlib', 'scipy',
+                      'pygmo', 'sympy', 'sphinx',
+                      'sphinxcontrib-seqdiag',
+                      'sphinx_rtd_theme',
+                      'recommonmark'],
+    url='https://github.com/MasashiSode',
+    # license=license,
+    packages=find_packages(exclude=['tests', 'docs']),
+)
