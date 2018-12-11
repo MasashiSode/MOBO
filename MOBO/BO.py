2d2a366bc86ae10db361ae82af82085cc3cf2646
Masashi Sode <masashi.sode@gmail.com>
2018-12-11 20:39:39 +0900

    kmeans added

--- MOBO/BO.py
+++ MOBO/BO.py
@@ -4,6 +4,7 @@ import numpy as np
 import pygmo as pg
 # from pygmo.problem import base
 import sklearn.gaussian_process as gp
+from sklearn.cluster import KMeans as km
 import multiprocessing as mp
 from matplotlib import pyplot as plt
 
@@ -15,7 +16,7 @@ class MultiObjectiveBayesianOptimization(object):
 
         pass
 
-    def set_train_data(self, x_observed, y_observed):
+    def set_train_data(self, x_observed, y_observed, n_cons=0):
         '''
         Args:
             x_observed: np.array (n_samples, n_params)
@@ -31,12 +32,15 @@ class MultiObjectiveBayesianOptimization(object):
             raise ValueError
         if not isinstance(y_observed, np.ndarray):
             raise ValueError
+        if n_cons is not 0:
+            self.flag_cons = True
 
         self.y_observed = y_observed
         self.x_observed = x_observed
         self.n_features = x_observed.shape[0]
         self.n_params = x_observed.shape[1]
-        self.n_obj = y_observed.shape[1]
+        self.n_obj = y_observed.shape[1] - n_cons
+        self.n_cons = n_cons
         self.bounds = [[min(x_observed[0]), min(x_observed[1])],
                        [max(x_observed[0]), max(x_observed[1])]]
         self.optimum_direction = -1 * np.ones(self.n_obj)
@@ -86,10 +90,12 @@ class MultiObjectiveBayesianOptimization(object):
             mobo.train_GPModel()
         '''
         self.mogp = MOBO.MOGP()
-        self.mogp.set_train_data(self.x_observed, self.y_observed)
+        self.mogp.set_train_data(
+            self.x_observed, self.y_observed, n_cons=self.n_cons)
         self.mogp.set_optimum_direction(self.optimum_direction)
         self.mogp.set_number_of_cpu_core(self.n_multiprocessing)
         self.mogp.train()
+        print('training done.')
         return
 
     def run_moga(self, size=48, gen=100):
@@ -112,6 +118,97 @@ class MultiObjectiveBayesianOptimization(object):
         self.pop = pg.population(self.prob, size=size)
         self.algo = pg.algorithm(pg.nsga2(gen=gen))
         self.pop = self.algo.evolve(self.pop)
+
+        self.non_dominated_fronts, self.domination_list, self.domination_count, self.non_domination_ranks = \
+            pg.fast_non_dominated_sorting(
+                self.pop.get_f())
+        print('moga done.')
+        return
+
+    def run_kmeans(self, n_clusters=8, init='k-means++', n_init=10, max_iter=300,
+               tol=0.0001, precompute_distances='auto', verbose=0,
+               random_state=None, copy_x=True, n_jobs=1):
+        '''
+        clustering parate front solutions by k-means
+
+        Args:
+            n_clusters : int, optional, default: 8
+                The number of clusters to form as well as the number of
+                centroids to generate.
+            init : {'k-means++', 'random' or an ndarray}
+                Method for initialization, defaults to 'k-means++':
+                'k-means++' : selects initial cluster centers for k-mean
+                clustering in a smart way to speed up convergence. See section
+                Notes in k_init for more details.
+                'random': choose k observations (rows) at random from data for
+                the initial centroids.
+                If an ndarray is passed, it should be of shape (n_clusters, n_features)
+                and gives the initial centers.
+            n_init : int, default: 10
+                Number of time the k-means algorithm will be run with different
+                centroid seeds. The final results will be the best output of
+                n_init consecutive runs in terms of inertia.
+            max_iter : int, default: 300
+                Maximum number of iterations of the k-means algorithm for a
+                single run.
+            tol : float, default: 1e-4
+                Relative tolerance with regards to inertia to declare convergence
+            precompute_distances : {'auto', True, False}
+                Precompute distances (faster but takes more memory).
+                'auto' : do not precompute distances if n_samples * n_clusters > 12
+                million. This corresponds to about 100MB overhead per job using
+                double precision.
+                True : always precompute distances
+                False : never precompute distances
+            verbose : int, default 0
+                Verbosity mode.
+            random_state : int, RandomState instance or None (default)
+                Determines random number generation for centroid initialization. Use
+                an int to make the randomness deterministic.
+                See :term:`Glossary <random_state>`.
+            copy_x : boolean, optional
+                When pre-computing distances it is more numerically accurate to center
+                the data first.  If copy_x is True (default), then the original data is
+                not modified, ensuring X is C-contiguous.  If False, the original data
+                is modified, and put back before the function returns, but small
+                numerical differences may be introduced by subtracting and then adding
+                the data mean, in this case it will also not ensure that data is
+                C-contiguous which may cause a significant slowdown.
+            n_jobs : int or None, optional (default=None)
+                The number of jobs to use for the computation. This works by computing
+                each of the n_init runs in parallel.
+                ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
+                ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
+                for more details.
+            algorithm : "auto", "full" or "elkan", default="auto"
+                K-means algorithm to use. The classical EM-style algorithm is "full".
+                The "elkan" variation is more efficient by using the triangle
+                inequality, but currently doesn't support sparse data. "auto" chooses
+                "elkan" for dense data and "full" for sparse data.
+
+        Examples::
+
+            mobo = MOBO.MultiObjectiveBayesianOptimization()
+            mobo.set_train_data(x_observed, y_observed)
+            mobo.train_GPModel()
+
+        See Also:
+            https://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html
+        '''
+
+        self.kmeans_clustering = km(
+            n_clusters=n_clusters, init=init, n_init=n_init, max_iter=max_iter,
+            tol=tol, precompute_distances=precompute_distances, verbose=verbose,
+            random_state=random_state, copy_x=copy_x, n_jobs=n_jobs)
+
+        X = self.pop.get_x()
+        Y = self.pop.get_f()
+        X = np.array(X[tuple(self.non_dominated_fronts)])
+        Y = np.array(Y[tuple(self.non_dominated_fronts)])
+        X = np.concatenate((X, Y), axis=1)
+        self.kmeans_clustering.fit(X)
+        self.kmeans_centroids = self.kmeans_clustering.cluster_centers_
+        print('kmeans fitting done.')
         return
 
 
--- test_MOGPOpt.py
+++ test_MOGPOpt.py
@@ -1,9 +1,9 @@
 import MOBO
 import numpy as np
 # import multiprocessing as mp
-import pygmo as pg
+# import pygmo as pg
 # from pygmo.problem import base
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
 
 
 def ReadInput(InputFile):
@@ -16,12 +16,11 @@ if __name__ == "__main__":
     x_observed = ReadInput('InputVar.csv')
 
     mobo = MOBO.MultiObjectiveBayesianOptimization()
-    mobo.set_train_data(x_observed, y_observed)
+    mobo.set_train_data(x_observed, y_observed, n_cons=0)
     mobo.train_GPModel()
     mobo.run_moga()
+    mobo.run_kmeans()
+    print(mobo.kmeans_centroids)
 
-    ndf, dl, dc, ndr = pg.fast_non_dominated_sorting(mobo.pop.get_f())
-    print(ndf, dc, ndr)
-
-    ax = pg.plot_non_dominated_fronts(mobo.pop.get_f())
-    plt.show()
+    # ax = pg.plot_non_dominated_fronts(mobo.pop.get_f())
+    # plt.show()
