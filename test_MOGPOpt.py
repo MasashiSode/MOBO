145dfc4f12fd7d326637caf08b9fa27f357185da
Masashi Sode <masashi.sode@gmail.com>
2018-12-11 22:00:57 +0900

    comment added

--- test_MOGPOpt.py
+++ test_MOGPOpt.py
@@ -17,8 +17,14 @@ if __name__ == "__main__":
 
     mobo = MOBO.MultiObjectiveBayesianOptimization()
     mobo.set_train_data(x_observed, y_observed, n_cons=0)
+
+    # training Gaussian Process regression
     mobo.train_GPModel()
+
+    # multi-objective optimization(nsga2) on surrogate model
     mobo.run_moga()
+
+    # clustering pareto front solutions
     mobo.run_kmeans()
     print(mobo.kmeans_centroids)
 
