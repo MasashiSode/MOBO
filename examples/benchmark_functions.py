7943c0239fab85494c6f4460860f7f9931ced2db
Masashi Sode <masashi.sode@gmail.com>
2018-12-13 16:45:38 +0900

    ZDT 2-6 added

--- examples/benchmark_functions.py
+++ examples/benchmark_functions.py
@@ -19,57 +19,81 @@ def ZDT1(x):
     return (x[:, 0], g * h)
 
 
-# def ZDT2(x):
-#     # m = 30 , x_i in [0,1]
-#     n_samples = x.shape[0]
-#     n_dv = x.shape[1]
-#     m = len(x)
-#     g = 1 + 9 * sum(x[1:]) / (m - 1)
-#     h = 1 - (x[0] / g) ** 2
-#     return (x[0], g * h)
+def ZDT2(x):
+    # m = 30 , x_i in [0,1]
+    n_samples = x.shape[0]
+    n_dv = x.shape[1]
+
+    g = np.zeros(n_samples)
+    h = np.zeros(n_samples)
+    for i in range(0, n_samples):
+        g[i] = 1 + 9 * sum(x[i, 1:]) / (n_dv - 1)
+        h[i] = 1 - (x[i, 0] / g) ** 2
+    return [x[0], g * h]
 
 
-# def ZDT3(x):
-#     # m = 30 , x_i in [0,1]
-#     n_samples = x.shape[0]
-#     n_dv = x.shape[1]
-#     m = len(x)
-#     g = 1 + 9 * sum(x[1:]) / (m - 1)
-#     h = 1 - math.sqrt(x[0] / g) - (x[0] / g) * math.sin(10 * math.pi * x[0])
-#     return (x[0], g * h)
+def ZDT3(x):
+    # m = 30 , x_i in [0,1]
+    n_samples = x.shape[0]
+    n_dv = x.shape[1]
+
+    g = np.zeros(n_samples)
+    h = np.zeros(n_samples)
+    for i in range(0, n_samples):
+        g[i] = 1 + 9 * sum(x[i, 1:]) / (n_dv - 1)
+        h[i] = 1 - np.sqrt(x[i, 0] / g[i]) - (x[i, 0] /
+                                              g[i]) * np.sin(10 * np.pi * x[0])
+    return [x[0], g * h]
 
 
-# def ZDT4(x):
-#     # m = 10 , x_1 in [0,1] , x_2,...,x_m in [-5,5]
-#     n_samples = x.shape[0]
-#     n_dv = x.shape[1]
-#     m = len(x)
-#     g = 1 + 10 * (m - 1) + \
-#         sum([(t ** 2 - 10 * math.cos(4 * math.pi * t)) for t in x[1:]])
-#     h = 1 - math.sqrt(x[0] / g)
-#     return (x[0], g * h)
+def ZDT4(x):
+    # m = 10 , x_1 in [0,1] , x_2,...,x_m in [-5,5]
+    n_samples = x.shape[0]
+    n_dv = x.shape[1]
+
+    g = np.zeros(n_samples)
+    h = np.zeros(n_samples)
+    for i in range(0, n_samples):
+        g[i] = 1 + 10 * (n_dv - 1) + \
+            sum([(t ** 2 - 10 * np.cos(4 * np.pi * t)) for t in x[i, 1:]])
+        h[i] = 1 - np.sqrt(x[i, 0] / g[i])
+    return [x[0], g * h]
 
 
-# def ZDT5(x):
-#     # m = 11 , x_1={0,1}^30 , x_2,...,x_m={0,1}^5
-#     f1 = 1 + x[0].count('1')
-#     g = 0
-#     for i in x[1:]:
-#         if i.count('1') < 5:
-#             g += 2 + i.count('1')
-#         elif i.count('1') == 5:
-#             g += 1
-#     h = 1 / f1
-#     return (f1, g * h)
+def ZDT5(x):
+    # m = 11 , x_1={0,1}^30 , x_2,...,x_m={0,1}^5
+    n_samples = x.shape[0]
+    n_dv = x.shape[1]
+
+    f = np.zeros(n_samples)
+    g = np.zeros(n_samples)
+    h = np.zeros(n_samples)
+
+    for i in range(0, n_samples):
+        f1[i] = 1 + x[i, 0].count('1')
+        for i in x[i, 1:]:
+            if i.count('1') < 5:
+                g[i] += 2 + i.count('1')
+            elif i.count('1') == 5:
+                g[i] += 1
+        h[i] = 1 / f1[i]
+    return [f1, g * h]
 
 
-# def ZDT6(x):
-#     # m = 10 , x_i in [0,1]
-#     m = len(x)
-#     f1 = 1 - math.exp(-4 * x[0]) * math.sin(6 * math.pi * x[0]) ** 6
-#     g = 1 + 9 * (sum(x[1:]) / (m - 1)) ** 0.25
-#     h = 1 - (f1 / g) ** 2
-#     return (f1, g * h)
+def ZDT6(x):
+    # m = 10 , x_i in [0,1]
+    n_samples = x.shape[0]
+    n_dv = x.shape[1]
+
+    f1 = np.zeros(n_samples)
+    g = np.zeros(n_samples)
+    h = np.zeros(n_samples)
+
+    for i in range(0, n_samples):
+        f1[i] = 1 - np.exp(-4 * x[i, 0]) * np.sin(6 * np.pi * x[i, 0]) ** 6
+        g[i] = 1 + 9 * (sum(x[i, 1:]) / (n_dv - 1)) ** 0.25
+        h[i] = 1 - (f1[i] / g[i]) ** 2
+    return [f1, g * h]
 
 
 if __name__ == "__main__":
