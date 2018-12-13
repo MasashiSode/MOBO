409fb0d9551a3c34982d73915dabb801cb44edd6
Masashi Sode <masashi.sode@gmail.com>
2018-12-13 19:55:43 +0900

    fixed

--- examples/ex_MultiObjectiveGP.py
+++ examples/ex_MultiObjectiveGP.py
@@ -25,9 +25,9 @@ if __name__ == "__main__":
     mobo = MOGP.MultiObjectiveBayesianOptimization()
     mobo.run_mobo(func=func, args=[],
                   n_dv=n_dv, n_obj=n_obj,
-                  n_init_lhs_samples=24,
-                  n_iter=10, n_new_ind=16,
-                  ga_pop_size=100, ga_gen=50, n_cons=0)
+                  n_init_lhs_samples=n_init_lhs_samples,
+                  n_iter=n_iter, n_new_ind=n_new_ind,
+                  ga_pop_size=ga_pop_size, ga_gen=ga_gen, n_cons=0)
 
     np.savetxt('ZDT' + str(n_zdt) + '_obj_res.csv',
                mobo.y_observed, delimiter=',')
