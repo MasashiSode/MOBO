03e1dfdb59417b5cfee3625ceaeab9630413a8b3
Masashi Sode <masashi.sode@gmail.com>
2018-12-13 18:48:44 +0900

    ZDT class moved into library

---
+++ MOGP/TestFunctions.py
@@ -0,0 +1,110 @@
+# https://sop.tik.ee.ethz.ch/download/supplementary/testproblems/
+import numpy as np
+
+
+class ZDT(object):
+    def __init__(self):
+        self.func = [self.ZDT1,
+                     self.ZDT2,
+                     self.ZDT3,
+                     None,
+                     None,
+                     self.ZDT6]
+        return
+
+    def get_obj_values(self, x, n_zdt=1):
+        if n_zdt == 5 or n_zdt == 4:
+            raise ValueError
+        self.n_zdt = n_zdt - 1
+        return self.func[self.n_zdt](x)
+
+    def get_func(self, n_zdt=1):
+        if n_zdt == 5 or n_zdt == 4:
+            raise ValueError
+        self.n_zdt = n_zdt - 1
+        return self.func[self.n_zdt]
+
+    def ZDT1(self, x):
+        # m = 30 , x_i in [0,1]
+        n_samples = x.shape[0]
+        n_dv = x.shape[1]
+
+        g = np.zeros(n_samples)
+        h = np.zeros(n_samples)
+        for i in range(0, n_samples):
+            g[i] = 1 + 9 * sum(x[i, 1:]) / (n_dv - 1)
+            h[i] = 1 - np.sqrt(x[i, 0] / g[i])
+        return [x[:, 0], g * h]
+
+    def ZDT2(self, x):
+        # m = 30 , x_i in [0,1]
+        n_samples = x.shape[0]
+        n_dv = x.shape[1]
+
+        g = np.zeros(n_samples)
+        h = np.zeros(n_samples)
+        for i in range(0, n_samples):
+            g[i] = 1 + 9 * sum(x[i, 1:]) / (n_dv - 1)
+            h[i] = 1 - (x[i, 0] / g[i]) ** 2
+        return [x[:, 0], g * h]
+
+    def ZDT3(self, x):
+        # m = 30 , x_i in [0,1]
+        n_samples = x.shape[0]
+        n_dv = x.shape[1]
+
+        g = np.zeros(n_samples)
+        h = np.zeros(n_samples)
+        for i in range(0, n_samples):
+            g[i] = 1 + 9 * sum(x[i, 1:]) / (n_dv - 1)
+            h[i] = 1 - np.sqrt(x[i, 0] / g[i]) - \
+                (x[i, 0] / g[i]) * np.sin(10 * np.pi * x[i, 0])
+        return [x[:, 0], g * h]
+
+    # def ZDT4(self, x):
+    #     # m = 10 , x_1 in [0,1] , x_2,...,x_m in [-5,5]
+    #     n_samples = x.shape[0]
+    #     n_dv = x.shape[1]
+
+    #     g = np.zeros(n_samples)
+    #     h = np.zeros(n_samples)
+    #     for i in range(0, n_samples):
+    #         g[i] = 1 + 10 * (n_dv - 1) + \
+    #             sum([(t ** 2 - 10 * np.cos(4 * np.pi * t))
+    #                  for t in x[i, 1:]])
+    #         h[i] = 1 - np.sqrt(x[i, 0] / g[i])
+    #     return [x[:, 0], g * h]
+
+    # def ZDT5(self, x):
+    #     # m = 11 , x_1={0,1}^30 , x_2,...,x_m={0,1}^5
+    #     n_samples = x.shape[0]
+
+    #     f1 = np.zeros(n_samples)
+    #     g = np.zeros(n_samples)
+    #     h = np.zeros(n_samples)
+
+    #     for i in range(0, n_samples):
+    #         f1[i] = 1 + x[i, 0].count('1')
+    #         for i in x[i, 1:]:
+    #             if i.count('1') < 5:
+    #                 g[i] += 2 + i.count('1')
+    #             elif i.count('1') == 5:
+    #                 g[i] += 1
+    #         h[i] = 1 / f1[i]
+    #     return [f1, g * h]
+
+    def ZDT6(self, x):
+        # m = 10 , x_i in [0,1]
+        n_samples = x.shape[0]
+        n_dv = x.shape[1]
+
+        f1 = np.zeros(n_samples)
+        g = np.zeros(n_samples)
+        h = np.zeros(n_samples)
+
+        for i in range(0, n_samples):
+            f1[i] = 1 - np.exp(-4 * x[i, 0]) * \
+                (np.sin(6 * np.pi * x[i, 0]) ** 6)
+            g[i] = 1 + 9 * (sum(x[i, 1:]) / (n_dv - 1)) ** 0.25
+            h[i] = 1 - (f1[i] / g[i]) ** 2
+        return [f1, h]
