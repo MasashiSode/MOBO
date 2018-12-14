62e7937e918cc6e0dce139c3f63ae2e87f865aa6
Masashi Sode <masashi.sode@gmail.com>
2018-12-14 20:40:17 +0900

    example changed

--- examples/ex_MultiObjectiveBayesianOptimization_with_constraints.py
+++ examples/ex_MultiObjectiveBayesianOptimization_with_constraints.py
@@ -6,19 +6,19 @@ from pyDOE import lhs
 
 if __name__ == "__main__":
 
-    n_init_lhs_samples = 100
+    n_init_lhs_samples = 24
 
     n_dv = 2
     n_obj_cons = 4
     n_cons = 2
 
-    n_iter = 10
-    n_new_ind = 8
+    n_iter = 16
+    n_new_ind = 4
 
     ga_pop_size = 100
     ga_gen = 50
 
-    mutation = 0.03
+    mutation = 0.04
 
     # user defined function y = f(x, args=[])
     func = MOGP.TestFunctions.ChakongHaimesFunction
@@ -38,12 +38,20 @@ if __name__ == "__main__":
                mobo.x_observed_org_coor, delimiter=',')
 
     index = np.all(mobo.y_observed_org_coor[:, n_obj_cons - n_cons + 1:n_obj_cons] < 0, axis=1)
+    # index_new_ind = np.all(mobo.y_observed_org_coor[n_init_lhs_samples:-1, n_obj_cons - n_cons + 1:n_obj_cons] < 0, axis=1)
+
     out_feasible = mobo.y_observed_org_coor[index]
+    # out_feasible_new_ind = mobo.y_observed_org_coor[index_new_ind]
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
     plt.grid(True)
     plt.scatter(mobo.y_observed_org_coor[:, 0], mobo.y_observed_org_coor[:, 1], label='infeasible')
     plt.scatter(out_feasible[:, 0], out_feasible[:, 1], label='feasible')
+    # plt.scatter(mobo.y_observed_org_coor[n_init_lhs_samples:-1, 0],
+    #             mobo.y_observed_org_coor[n_init_lhs_samples:-1, 1],
+    #             label='new individuals')
+    # plt.scatter(out_feasible_new_ind[:, 0], out_feasible_new_ind[:, 1], label='feasible new individuals')
+
     ax.legend()
     plt.show()
