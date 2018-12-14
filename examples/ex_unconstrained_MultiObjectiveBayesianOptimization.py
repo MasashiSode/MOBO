1aa3038c1fbfa2ab02be1c92b428a14c7a414c52
Masashi Sode <masashi.sode@gmail.com>
2018-12-15 00:47:19 +0900

    bug fixed

--- examples/ex_unconstrained_MultiObjectiveBayesianOptimization.py
+++ examples/ex_unconstrained_MultiObjectiveBayesianOptimization.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     n_init_lhs_samples = 48
 
     n_dv = 2
-    n_obj = 2
+    n_obj_cons = 2
 
     n_iter = 10
     n_new_ind = 16
@@ -24,16 +24,16 @@ if __name__ == "__main__":
 
     mobo = MOGP.MultiObjectiveBayesianOptimization()
     mobo.run_mobo(func=func, args=[],
-                  n_dv=n_dv, n_obj=n_obj,
+                  n_dv=n_dv, n_obj_cons=n_obj_cons,
                   n_init_lhs_samples=n_init_lhs_samples,
                   n_iter=n_iter, n_new_ind=n_new_ind,
                   ga_pop_size=ga_pop_size, ga_gen=ga_gen, n_cons=0)
 
     np.savetxt('ZDT' + str(n_zdt) + '_obj_res.csv',
-               mobo.y_observed, delimiter=',')
+               mobo.y_observed_org_coor, delimiter=',')
     np.savetxt('ZDT' + str(n_zdt) + '_var_res.csv',
-               mobo.x_observed, delimiter=',')
+               mobo.x_observed_org_coor, delimiter=',')
 
     plt.grid(True)
-    plt.scatter(mobo.y_observed[:, 0], mobo.y_observed[:, 1])
+    plt.scatter(mobo.y_observed_org_coor[:, 0], mobo.y_observed_org_coor[:, 1])
     plt.show()
