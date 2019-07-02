6c8a86c19bf2a3c2fc36f0888c6e92c6520a4789
Masashi Sode <masashi.sode@gmail.com>
2019-07-02 20:54:28 +0900

    nsga2 with deap implemented (WIP)

--- mobo/test_functions/multi_objective_functions.py
+++ mobo/test_functions/multi_objective_functions.py
@@ -1,155 +1,138 @@
-import copy
-import numpy as np
+import array
+import random
+import json
+
+import numpy
+
+from math import sqrt
+
+from deap import algorithms
+from deap import base
+from deap import benchmarks
+from deap.benchmarks.tools import diversity, convergence, hypervolume
+from deap import creator
+from deap import tools
 
 
-def zdt1(x: np.ndarray) -> np.ndarray:
-    """
-    Args:
-        x (np.ndarray): x must be in [0, 1]
-    Returns:
-        multi_objective_value (np.ndarray):
-    """
-    if type(x) is not np.ndarray:
-        x_in = np.array(x)
-    else:
-        x_in = copy.deepcopy(x)
-    if np.any(x_in > 1) or np.any(x_in < 0):
-        raise ValueError('0 <= x <= 1 is required')
-
-    n_dv = x_in.shape[0]
-    g = 1 + 9 * sum(x_in[1:]) / (n_dv - 1)
-    h = 1 - np.sqrt(x_in[0] / g)
-    f1 = x_in[0]
-    f2 = g * h
-    return np.array([f1, f2])
+def uniform(low, up, size=None):
+    try:
+        return [random.uniform(a, b) for a, b in zip(low, up)]
+    except TypeError:
+        return [random.uniform(a, b) for a, b in zip([low] * size, [up] * size)]
 
 
-def zdt2(x: np.ndarray) -> np.ndarray:
-    """
-    Args:
-        x (np.ndarray): x must be in [0, 1]
-    Returns:
-        multi_objective_value (np.ndarray):
-    """
-    if type(x) is not np.ndarray:
-        x_in = np.array(x)
-    else:
-        x_in = copy.deepcopy(x)
-    if np.any(x_in > 1) or np.any(x_in < 0):
-        raise ValueError('0 <= x <= 1 is required')
+class NSGA2():
+    def __init__(self, evaluation_function=None,
+                 bound_low=0.0, bound_up=1.0,
+                 n_dimension=30, mu=24,
+                 gen=100, cxpb=0.9,
+                 seed=9):
+        random.seed(seed)
+        self.toolbox = base.Toolbox()
+        self.bound_low = bound_low
+        self.bound_up = bound_up
+        self.n_dimension = n_dimension
+        self.mu = mu
+        self.gen = gen
+        self.cxpb = cxpb
+        self.evaluation_function = evaluation_function
 
-    n_dv = x_in.shape[0]
-    g = 1 + 9 * sum(x_in[1:]) / (n_dv - 1)
-    h = 1 - (x_in[0] / g) ** 2
-    f1 = x_in[0]
-    f2 = g * h
-    return np.array([f1, f2])
+        creator.create("FitnessMin", base.Fitness, weights=(-1.0, -1.0))
+        creator.create("Individual", array.array, typecode='d',
+                       fitness=creator.FitnessMin)
 
+        self.toolbox.register("attr_float", uniform,
+                              self.bound_low, self.bound_up,
+                              self.n_dimension)
+        self.toolbox.register("individual", tools.initIterate,
+                              creator.Individual, self.toolbox.attr_float)
+        self.toolbox.register("population", tools.initRepeat,
+                              list, self.toolbox.individual)
 
-def schaffer_n1(x: np.ndarray) -> np.ndarray:
-    """
-    Args:
-        x (np.ndarray): x must be in [0, 1]
-    Returns:
-        multi_objective_value (np.ndarray):
-    """
-    if type(x) is not np.ndarray:
-        x_in = np.array(x)
-    else:
-        x_in = copy.deepcopy(x)
-    if np.any(x_in > 1) or np.any(x_in < 0):
-        raise ValueError('0 <= x <= 1 is required')
+        self.toolbox.register("evaluate", self.evaluation_function)
+        self.toolbox.register("mate", tools.cxSimulatedBinaryBounded,
+                              low=self.bound_low, up=self.bound_up, eta=20.0)
+        self.toolbox.register("mutate", tools.mutPolynomialBounded,
+                              low=self.bound_low, up=self.bound_up, eta=20.0, indpb=1.0 / self.n_dimension)
+        self.toolbox.register("select", tools.selNSGA2)
 
-    f1 = x[0]**2
-    f2 = (x[0]-2)**2
-    return np.array([f1, f2])
+        self.stats = tools.Statistics(lambda ind: ind.fitness.values)
+        self.stats.register("avg", numpy.mean, axis=0)
+        self.stats.register("std", numpy.std, axis=0)
+        self.stats.register("min", numpy.min, axis=0)
+        self.stats.register("max", numpy.max, axis=0)
+        self.logbook = tools.Logbook()
+        self.logbook.header = "gen", "evals", "std", "min", "avg", "max"
+        self.pop = self.toolbox.population(n=self.mu)
 
+    def run(self):
+        # Evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in self.pop if not ind.fitness.valid]
+        fitnesses = self.toolbox.map(self.toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
 
-def binh_korn(x: np.ndarray) -> np.ndarray:
-    """
-    Args:
-        x (np.ndarray): x must be in [0, 1]
-    Returns:
-        multi_objective_value (np.ndarray):
-    """
-    if type(x) is not np.ndarray:
-        x_in = np.array(x)
-    else:
-        x_in = copy.deepcopy(x)
-    if np.any(x_in > 1) or np.any(x_in < 0):
-        raise ValueError('0 <= x <= 1 is required')
+        # This is just to assign the crowding distance to the individuals
+        # no actual selection is done
+        self.pop = self.toolbox.select(self.pop, len(self.pop))
 
-    x_in = copy.deepcopy(x)
-    x_in[0] = x_in[0] * 5
-    x_in[1] = x_in[1] * 3
+        record = self.stats.compile(self.pop)
+        self.logbook.record(gen=0, evals=len(invalid_ind), **record)
+        print(self.logbook.stream)
 
-    f1 = 4 * (x_in[0] ** 2) + 4 * (x_in[1] ** 2)
-    f2 = (x_in[0] - 5) ** 2 + (x_in[1] - 5) ** 2
-    g1 = (x_in[0] - 5) ** 2 + x_in[1] ** 2 - 25
-    g2 = 7.7 - (x_in[0] - 8) ** 2 + (x_in[1] + 3) ** 2
-    return np.array([f1, f2, g1, g2])
+        # Begin the generational process
+        for gen in range(1, self.gen):
+            # Vary the population
+            offspring = tools.selTournamentDCD(self.pop, len(self.pop))
+            offspring = [self.toolbox.clone(ind) for ind in offspring]
 
+            for ind1, ind2 in zip(offspring[::2], offspring[1::2]):
+                if random.random() <= self.cxpb:
+                    self.toolbox.mate(ind1, ind2)
 
-def chakong_haimes(x: np.ndarray) -> np.ndarray:
-    """
-    Args:
-        x (np.ndarray): x must be in [0, 1]
-    Returns:
-        multi_objective_value (np.ndarray):
-    """
-    if type(x) is not np.ndarray:
-        x_in = np.array(x)
-    else:
-        x_in = copy.deepcopy(x)
-    if np.any(x_in > 1) or np.any(x_in < 0):
-        raise ValueError('0 <= x <= 1 is required')
-    x_in = copy.deepcopy(x)
-    x_in[0] = x_in[0] * 40 - 20
-    x_in[1] = x_in[1] * 40 - 20
+                self.toolbox.mutate(ind1)
+                self.toolbox.mutate(ind2)
+                del ind1.fitness.values, ind2.fitness.values
 
-    f1 = 2 + (x_in[0] - 2) ** 2 + (x_in[1] - 1) ** 2
-    f2 = 9 * x_in[0] - (x_in[1] - 1) ** 2
-    g1 = x_in[0] ** 2 + x_in[1] ** 2 - 255
-    g2 = x_in[0] - 3 * x_in[1] + 10
+            # Evaluate the individuals with an invalid fitness
+            invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+            fitnesses = self.toolbox.map(self.toolbox.evaluate, invalid_ind)
+            for ind, fit in zip(invalid_ind, fitnesses):
+                ind.fitness.values = fit
 
-    return np.array([f1, f2, g1, g2])
+            # Select the next generation population
+            self.pop = self.toolbox.select(self.pop + offspring, self.mu)
+            record = self.stats.compile(self.pop)
+            self.logbook.record(gen=gen, evals=len(invalid_ind), **record)
+            print(self.logbook.stream)
 
-
-def osyczka_kundu(x: np.ndarray) -> np.ndarray:
-    """
-    Args:
-        x (np.ndarray): x must be in [0, 1]
-    Returns:
-        multi_objective_value (np.ndarray):
-    """
-    if type(x) is not np.ndarray:
-        x_in = np.array(x)
-    else:
-        x_in = copy.deepcopy(x)
-    if np.any(x_in > 1) or np.any(x_in < 0):
-        raise ValueError('0 <= x <= 1 is required')
-    x_in = copy.deepcopy(x)
-    x_in[0] = x_in[0] * 10
-    x_in[1] = x_in[1] * 10
-    x_in[2] = x_in[2] * 4 + 1
-    x_in[3] = x_in[3] * 6
-    x_in[4] = x_in[4] * 4 + 1
-    x_in[5] = x_in[5] * 10
-
-    f1 = -25 * (x_in[0] - 2) ** 2 - (x_in[1] - 2) ** 2
-    - (x_in[2] - 1) ** 2 - (x_in[3] - 4) ** 2
-    - (x_in[4] - 1) ** 2
-    f2 = 0
-    for i in range(0, 6):
-        f2 = f2 + x_in[i] ** 2
-    g1 = (x_in[0] + x_in[1] - 2)
-    g2 = (6 - x_in[0] - x_in[1])
-    g3 = (2 + x_in[1] - x_in[0])
-    g4 = (2 - x_in[0] + 3 * x_in[1])
-    g5 = (4 - (x_in[2] - 3) ** 2 - x_in[3])
-    g6 = ((x_in[4] - 3) ** 2 + x_in[5] - 4)
-    return np.array([f1, f2, g1, g2, g3, g4, g5, g6])
+        print("Final population hypervolume is %f" %
+              hypervolume(self.pop, [11.0, 11.0]))
+        return self.pop, self.logbook
 
 
 if __name__ == "__main__":
-    pass
+    # with open("pareto_front/zdt1_front.json") as optimal_front_data:
+    #     optimal_front = json.load(optimal_front_data)
+    # # Use 500 of the 1000 points in the json file
+    # optimal_front = sorted(optimal_front[i]
+    #                        for i in range(0, len(optimal_front), 2))
+
+    # pop, stats = main()
+    nsga2 = NSGA2(evaluation_function=benchmarks.zdt1)
+    pop, stats = nsga2.run()
+    pop.sort(key=lambda x: x.fitness.values)
+
+    print(stats)
+    # print("Convergence: ", convergence(pop, optimal_front))
+    # print("Diversity: ", diversity(pop, optimal_front[0], optimal_front[-1]))
+
+    import matplotlib.pyplot as plt
+    # import numpy
+
+    front = numpy.array([ind.fitness.values for ind in pop])
+    # optimal_front = numpy.array(optimal_front)
+    # plt.scatter(optimal_front[:, 0], optimal_front[:, 1], c="r")
+    plt.scatter(front[:, 0], front[:, 1], c="b")
+    plt.axis("tight")
+    plt.show()
