2c97c15c8f2432c83e9cb01759640dbb5639a2e9
Masashi Sode <masashi.sode@gmail.com>
2019-07-03 20:12:10 +0900

    version updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mobo',
-    version='0.0.1',
+    version='0.0.2',
     description='Multi-objective bayesian optimization package',
     # long_description=readme,
     author='Masashi Sode',
