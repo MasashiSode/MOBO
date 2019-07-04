dde45f6037304a6acd02be2fc366cc9f0a742542
Masashi Sode <masashi.sode@gmail.com>
2019-07-04 20:07:53 +0900

    gp model added

---
+++ mobo/model/exact_gp.py
@@ -0,0 +1,16 @@
+import gpytorch
+from gpytorch.kernels import ScaleKernel
+from gpytorch.kernels import MaternKernel
+
+
+class ExactGPModel(gpytorch.models.ExactGP):
+    def __init__(self, train_x, train_y, likelihood):
+        super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
+        self.mean_module = gpytorch.means.ConstantMean()
+        self.covar_module = ScaleKernel(
+            MaternKernel(5/2, ard_num_dims=train_x.shape[1]))
+
+    def forward(self, x):
+        mean_x = self.mean_module(x)
+        covar_x = self.covar_module(x)
+        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
