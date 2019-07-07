7afdaa00c353cd29c5e05c1f6b333b8587cc029c
Masashi Sode <masashi.sode@gmail.com>
2019-07-07 21:34:17 +0900

    ucb implemented

--- tutorials/multi_objective_optimization_example.py
+++ tutorials/multi_objective_optimization_example.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 
 from mobo.optimizer import NSGA2
 from mobo.model import ExactGPModel
-from mobo.acquisition import ei
+# from mobo.acquisition import ei
+from mobo.acquisition import ucb
+
 from mobo.bayesopt import MultiObjectiveBayesianOpt
 from mobo.test_functions import zdt1
 
@@ -11,20 +13,22 @@ if __name__ == "__main__":
 
     # multi objective genetic algorithm (NSGA2) is implemented with 'DEAP'
     # Gaussian Process model is implemented with 'gpytorch'
-    opt = MultiObjectiveBayesianOpt(evaluation_function=zdt1,
-                                    surrogate_model=ExactGPModel,
-                                    optimizer=NSGA2,
-                                    acquisition=ei,
-                                    n_objective_dimension=2,
-                                    n_design_variables_dimension=30,
-                                    n_initial_sample=64,
-                                    bayesian_optimization_iter_max=3,
-                                    likelihood_optimization_iter_max=5000,
-                                    likelihood_optimization_criteria=1e-8,
-                                    n_new_samples=16,
-                                    n_ga_population=100,
-                                    n_ga_generation=100,
-                                    )
+    opt = MultiObjectiveBayesianOpt(
+        evaluation_function=zdt1,
+        surrogate_model=ExactGPModel,
+        optimizer=NSGA2,
+        # acquisition=ei,
+        acquisition=ucb,
+        n_objective_dimension=2,
+        n_design_variables_dimension=30,
+        n_initial_sample=64,
+        n_new_samples=8,
+        bayesian_optimization_iter_max=8,
+        likelihood_optimization_iter_max=5000,
+        likelihood_optimization_criteria=1e-6,
+        n_ga_population=16,
+        n_ga_generation=100,
+    )
     result = opt.optimize()
 
     front = np.array(result[1])
